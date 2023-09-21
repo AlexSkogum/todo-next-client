22b012d30f8bb04a2f471658fcf3e3cdc6934b36
AlexSkogum <145666442+AlexSkogum@users.noreply.github.com>
2023-09-21 13:44:57 +0200

    Update useTodos.js

--- components/useTodos.js
+++ components/useTodos.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const SERVER_ENDPOINT = 'https://humble-space-giggle-4qw9qjp9rph7vwq-5000.app.github.dev'
+const SERVER_ENDPOINT = 'https://super-duper-winner-wr7q45jr7w75fvqg-5000.app.github.dev/'
 
 export const useTodos = () => {
   const [todos, setTodos] = useState([]);
